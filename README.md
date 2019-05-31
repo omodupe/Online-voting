d3b0a0b6ac9690695c52923ae71bc93d9a37a7c3
omodupe <olowolafemodupe@gmail.com>
2019-05-31 09:51:34 +0100

    Edit README

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Online-voting
+Online-voting is an electronic voting system for polling. The election process is in need of a standard and secure electronic system that voters can rely on and have trust in.
+
+The App is design in the way voters and political party candidates can register.
--- scripts/js/candidate.js
+++ scripts/js/candidate.js
@@ -1,15 +1,16 @@
+const api = "http://localhost:3000/Candidates";
+const datas = {
+    firstName: $("#FirstName").val(),
+    lastName: $("#LastName").val(),
+    party: $("#Party").val(),
+    password: $("#Password").val(),
+    phonenumber: $("#PhoneNumber").val(),
+}
 
 $(function () {
     $("form").submit(function (event) {
         event.preventDefault()
-        const datas = {
-            firstName: $("#FirstName").val(),
-            lastName: $("#LastName").val(),
-            party: $("#Party").val(),
-            password: $("#Password").val(),
-            phonenumber: $("#PhoneNumber").val(),
-        }
-        $.post("http://localhost:3000/Candidates", datas).done(function (data) {
+        $.post(api, datas).done(function (data) {
             console.log("saved")
             console.log(data)
         })
@@ -17,15 +18,43 @@ $(function () {
     })
 })
 
+// function displayCandidates(data) {
+//     if(!data) return;
+
+//     var obj= JSON.parse(data);
+
+//     obj.forEach((dat) => {
+//         let getFirstName= dat.firstName;
+//         let body= document.querySelector('display');
+//         body.innerText= getFirstName;
+//     })
+
+// }
+
+// function getCandidates() {
+
+//     fetch(api)
+//         .then(function (response) { return response.json(); })
+//         .then(function (data) {
+//             displayCandidates(data);
+//             console.log(data);
+//         })
+// }
+
+
+
+
+
+
 //Reset input fields
 function reset() {
-    let firstName= document.querySelector('#FirstName');
-    let lastName= document.querySelector('#LastName');
-    let party= document.querySelector('#Party');
-    let password= document.querySelector('#Password');
-    let phonenumber= document.querySelector('#PhoneNumber');
+    let firstName = document.querySelector('#FirstName');
+    let lastName = document.querySelector('#LastName');
+    let party = document.querySelector('#Party');
+    let password = document.querySelector('#Password');
+    let phonenumber = document.querySelector('#PhoneNumber');
 
-    let arrAll= [firstName, lastName, party, password, phonenumber];
+    let arrAll = [firstName, lastName, party, password, phonenumber];
     arrAll.forEach((input) => {
         return input.value = "";
     })
